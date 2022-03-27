f89c974a7d10d423002b47b90cf23a99ea05a82c
vibhav chirravuri <44679811+csvultrainstinct@users.noreply.github.com>
2022-03-27 16:52:22 +0530

    Create 71A - Way Too Long Words.cpp

---
+++ DivA/71A - Way Too Long Words.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    while(n--){
+        string s;
+        cin>>s;
+        int n=s.length();
+        if(n<=10){
+            cout<<s<<endl;
+        }
+        else{
+            cout<<s[0]<<n-2<<s[n-1]<<endl;
+        }
+    }
+    return 0;
+}
